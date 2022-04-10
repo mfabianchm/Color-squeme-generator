e06ce5529ca01c4badd01bcee35ec866a01f2379
Marcos Chong <mfchongm@hotmail.com>
2022-04-10 18:00:29 -0500

    added button funcionality

---
+++ script.js
@@ -0,0 +1,40 @@
+//Show the colors in the color bars
+
+
+const button = document.getElementById('button')
+const selectColorInput = document.getElementById('select-color-input')
+const modeSelectButton = document.getElementById('modeSelectButton')
+let optionsArray = Object.values(modeSelectButton)
+const colorBars = Object.values(document.querySelector('.scheme-colors').children)
+
+
+
+
+button.addEventListener('click', e => {
+    let hexColorSelected = selectColorInput.value
+    let n = hexColorSelected.slice(1)
+    let value
+
+    optionsArray.map(option => {
+       if(option.selected) {
+         value = option.value
+       }
+    })
+    
+    let url = `https://www.thecolorapi.com/scheme?hex=${n}&format=json&mode=${value}&count=5`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+
+        //Dependiendo el index y value poner el valor correspondiente en cada color bar
+        console.log(data)
+        let colors = data.colors
+        let idx = 0
+        colors.map(color => {
+          let colorHex = color.hex.value
+          colorBars[idx].style.backgroundColor = colorHex
+          idx++
+        })
+      })
+})
+    
