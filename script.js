e49a3dc62eaf5a316731fc8d98f0aebf4123c35a
Marcos Chong <mfchongm@hotmail.com>
2022-04-10 18:21:02 -0500

    added fetch when website loads to display colors in the scheme bars and text name app

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-//Show the colors in the color bars
-
 
 const button = document.getElementById('button')
 const selectColorInput = document.getElementById('select-color-input')
 const modeSelectButton = document.getElementById('modeSelectButton')
 let optionsArray = Object.values(modeSelectButton)
 const colorBars = Object.values(document.querySelector('.scheme-colors').children)
+const hexCodesText = Object.values(document.querySelector('.hex-names-container').children)
+
+
+fetch("https://www.thecolorapi.com/scheme?hex=000000&format=json&mode=monochrome&count=5")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        let colors = data.colors
+        console.log(colors)
+        let idx = 0
+        colors.map(color => {
+          let colorHex = color.hex.value
+          colorBars[idx].style.backgroundColor = colorHex
+          hexCodesText[idx].innerHTML = colorHex
+          idx++
+        })
+      })
+
 
 
 
@@ -25,14 +41,14 @@ button.addEventListener('click', e => {
     fetch(url)
       .then(response => response.json())
       .then(data => {
-
-        //Dependiendo el index y value poner el valor correspondiente en cada color bar
         console.log(data)
         let colors = data.colors
+        console.log(colors)
         let idx = 0
         colors.map(color => {
           let colorHex = color.hex.value
           colorBars[idx].style.backgroundColor = colorHex
+          hexCodesText[idx].innerHTML = colorHex
           idx++
         })
       })
